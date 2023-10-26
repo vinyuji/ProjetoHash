3ea650e4aa9d5ab12610354d84c8511c08aac893
Anthonygoc <anthonygabrieloliveiracruz@gmail.com>
2023-10-26 17:15:57 -0400

    Atualizado

--- hash.h
+++ hash.h
@@ -1,5 +1,19 @@
-typedef struct hash
-{
-    int valor;
-    char nome; 
-}
+#include "list.h"
+
+    typedef struct TabelaHash{
+    Lista hashes[100];
+    int tam;
+
+};
+TabelaHash;
+
+void initHash(TabelaHash *TabelhaHash);
+bool Tabelavazia(TabelaHash *TabelaHash);
+int hash(char *key);
+int puthash(TabelaHash *TabelaHash, char *key, void *data, compare equal);
+void* get (TabelaHash *TabelaHash, char *Key, compare equal);
+void* removekey(TabelaHash *TabelaHash, char *key, compare equal);
+void mostrarHash(TabelaHash *TabelaHash, printNode print);
+bool ChaveValor(TabelaHash *TabelaHash, char *key, compare equal);
+
+#endif
