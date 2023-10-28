7972f0493ebf3d7fd2b1a1c0c32b2c36feb660fe
Anthonygoc <anthonygabrieloliveiracruz@gmail.com>
2023-10-27 20:58:37 -0400

    Atualizado Anthony

--- Lista.h
+++ Lista.h
@@ -1,19 +1,28 @@
+#include "stdbool.h"
+
 typedef struct No {
-    void *Data;
+    void *data;
     struct No *Proximo;
 }No;
+
+
 
 typedef struct Lista{
     No *Inicio;
     int Tam;
 }Lista;
 
+typedef bool (*compare)(void*,void*);
+
+
 void Init(Lista *list);
-int InserirInicioLista(Lista *list, void* Data);
-int InserirUltimoLista(Lista *list, void* Data);
+int InserirInicioLista(Lista *list, void* data);
+int InserirUltimoLista(Lista *list, void* data);
 void* RemoverInicioLista(Lista *list);
 void* RemoverFinalLista(Lista *list);
+void* removeposicao(Lista *Lista, int posicao);
 void ShowLista(Lista *list);
 int Verificar(Lista *list);
+int indexOf(Lista *list, void *data, compare equal);
 
 
